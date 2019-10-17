25a1b1ae48bb31e38b04f45e987be74b2270b490
Anwesh <anwesh897@gmail.com>
2019-10-17 09:43:48 +0530

    commit

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const path = require("path");
 const fs = require("fs");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile("home.html", { root: __dirname });
-});
+// app.get("/", (req, res) => {
+//   res.sendFile("home.html", { root: __dirname });
+// });
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/thoughts", (req, res) => {
-  res.sendFile("thoughts.html", { root: __dirname });
+  res.sendFile("thoughts.html", { root: __dirname + "/public" });
 });
 
 const port = process.env.PORT || 3000;
